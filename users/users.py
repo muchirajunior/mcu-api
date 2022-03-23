44eeadbb2be150847526a5edce72f1bb734fe404
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2022-03-23 11:58:53 +0300

    login

--- users/users.py
+++ users/users.py
@@ -48,7 +48,7 @@ def loginUser():
         if user == None:
             return {"msg":"login failed, username does not exist"},406
         if bcrypt.check_password_hash(user['password'].encode('utf-8'),data['password']) :
-            user=user.pop('password')
+            user.pop("password")
             token=create_access_token(identity=user)
             return jsonify({"msg":"login successful","user":user,"token":token}),200
 

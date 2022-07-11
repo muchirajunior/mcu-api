4565b7029f6e5fb0082625694c4ce8adf41b77a8
MUCHIRA JUNIOR <56454064+muchirajunior@users.noreply.github.com>
2022-07-11 23:36:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,6 @@ def index():
 
     return {"message":"api running successfully"}
 
-@app.route('/ussd', methods=['POST','GET'])
-def index():
-    if request.method=="POST":
-        data = request.data.decode('utf-8')
-        return data+" junior"
-    return "message from muchira junior "
 
 
 # if __name__=="__main_ _":
